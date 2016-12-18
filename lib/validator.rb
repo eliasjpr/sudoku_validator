c06136a631cc0b0586f2c066df1302c2794d81db
Elias Perez <eperez@pinch.me>
2016-12-18 17:50:38 -0500

    refactor

--- lib/validator.rb
+++ lib/validator.rb
@@ -1,5 +1,9 @@
 # Validates a sudoku puzzle
 class Validator
+  VALID = 'This sudoku is valid.'.freeze
+  INVALID = 'This sudoku is invalid.'.freeze
+  VALID_INCOMPLETE = 'This sudoku is valid, but incomplete.'.freeze
+
   def initialize(puzzle_string)
     @grid = []
     @puzzle_string = puzzle_string
@@ -12,13 +16,20 @@ class Validator
   end
 
   def validate
-    valid = (validate_rows && validate_columns && validate_regions)
-    complete = complete?
-    return 'This sudoku is valid.' if valid && complete
-    return 'This sudoku is valid, but incomplete.' if valid && !complete
-    'This sudoku is invalid.'
+    return INVALID unless valid?
+    return VALID_INCOMPLETE unless complete?
+    VALID
   end
 
+  def valid?
+    (valid_rows? && valid_columns? && valid_regions?)
+  end
+
+  private
+
+  # This method parses sudoku files extensions
+  # with specifi output format, if output changes
+  # this method must be revised
   def build_grid(puzzle)
     invalid_row = '------+------+------'
     grid = puzzle.delete(invalid_row).tr('|', '').split(/\n/).reject!(&:empty?)
@@ -27,29 +38,27 @@ class Validator
     end
   end
 
-  private
-
-  def validate_rows
-    @grid.map { |r| dupe?(r) }.all?{ |e| e == false }
+  def valid_rows?
+    @grid.map { |r| duplicate?(r) }.all? { |e| e == false }
   end
 
-  def validate_columns
+  def valid_columns?
     new_grid = @grid.map(&:flatten)
-    new_grid.transpose.map { |c| dupe?(c) }.all?{ |e| e == false }
+    new_grid.transpose.map { |c| duplicate?(c) }.all? { |e| e == false }
   end
 
-  def validate_regions
+  def valid_regions?
     regions = []
-    @grid.each_slice(3).with_index do |region|
-      regions << dupe?(region.transpose[0])
-      regions << dupe?(region.transpose[1])
-      regions << dupe?(region.transpose[2])
+    @grid.each_slice(3) do |region|
+      (0..2).each do |i|
+        regions << duplicate?(region.transpose[i])
+      end
     end
     # All region must not have duplicates
     regions.all? { |e| e == false }
   end
 
-  def dupe?(arr)
+  def duplicate?(arr)
     result = arr.flatten.each_with_object({}) do |e, a|
       (a.key?(e) ? a[e] += 1 : a[e] = 0) if e > 0
     end
