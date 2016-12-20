62596e5e52edaa0dada8b7dc5ef99e70ce226875
Elias Perez <eperez@pinch.me>
2016-12-19 21:27:43 -0500

    updates

--- spec/end_to_end_integration_spec.rb
+++ spec/end_to_end_integration_spec.rb
@@ -27,17 +27,19 @@ describe "End-to-end test" do
     end
   end
 
-  context 'when the sudoku is invalid' do
-    invalid_fixtures = ['spec/fixtures/invalid_due_to_row_dupe.sudoku',
-                        'spec/fixtures/invalid_due_to_column_dupe.sudoku',
-                        'spec/fixtures/invalid_due_to_subgroup_dupe.sudoku']
+  context "when the sudoku is invalid" do
+    invalid_fixtures = ["spec/fixtures/invalid_due_to_row_dupe.sudoku",
+                        "spec/fixtures/invalid_due_to_column_dupe.sudoku",
+                        "spec/fixtures/invalid_due_to_subgroup_dupe.sudoku"]
 
     invalid_fixtures.each do |fixture|
-      it 'returns a string saying so' do
+      it "returns a string saying so" do
         result = Validator.validate(File.read(fixture))
+
         expect(result).to(
-          eq('This sudoku is invalid.'),
-          "Expected #{fixture} to be invalid but it wasn't.")
+          eq("This sudoku is invalid."),
+          "Expected #{fixture} to be invalid but it wasn't."
+        )
       end
     end
   end
